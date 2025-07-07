723ad070b0677409c04196b072090ab987df0300
Anthony Mogrovejo <kdetony@ballenaroja.services>
2025-07-07 14:34:50 -0500

    Update12 main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -4,8 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-      configuration_aliases = [azurerm.management]
+      version = ">= 3.87.0"  
     }
     random = {
       source  = "hashicorp/random"
@@ -16,8 +15,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  resource_provider_registrations = "all"
-  }
+  # Si usas azurerm < 3.87.0, comenta esta línea
+  # resource_provider_registrations = "all"
+}
 
 variable "tags" {
   default = {
@@ -32,12 +32,16 @@ variable "docker_image" {
   default     = "kdetony/webapp:0.1"
 }
 
-# Obtener el resource group existente
+variable "service_principal_object_id" {
+  description = "Object ID of the Azure Service Principal"
+  type        = string
+}
+
+# Obtener recursos existentes
 data "azurerm_resource_group" "rg" {
   name = "rg-webapp-github"
 }
 
-# Obtener el App Service Plan existente
 data "azurerm_app_service_plan" "asp" {
   name                = "asp-webapp-github"
   resource_group_name = data.azurerm_resource_group.rg.name
@@ -54,19 +58,21 @@ resource "azurerm_application_insights" "appinsights" {
   application_type    = "web"
 }
 
-resource "azurerm_app_service" "webapp" {
-  name                = "webapp-${random_id.unique.hex}"  # Nombre más corto
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "webapp-${random_id.unique.hex}"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  app_service_plan_id = data.azurerm_app_service_plan.asp.id
+  service_plan_id     = data.azurerm_app_service_plan.asp.id
 
   site_config {
-    linux_fx_version = "DOCKER|${var.docker_image}"
-    always_on        = false
-    health_check_path = "/"  # Ruta de health check obligatoria
-    
-    # Configuración de contenedores
-    container_registry_use_managed_identity = true
+    application_stack {
+      docker_image     = var.docker_image
+      docker_image_tag = "latest"
+    }
+
+    always_on         = true
+    health_check_path = "/"
+    ftps_state        = "Disabled"
   }
 
   identity {
@@ -77,24 +83,21 @@ resource "azurerm_app_service" "webapp" {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
     DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
     APPINSIGHTS_INSTRUMENTATIONKEY      = azurerm_application_insights.appinsights.instrumentation_key
-    
-    # Configuraciones recomendadas
-    WEBSITES_PORT                       = "8080"  # Puerto que usa tu contenedor
+    WEBSITES_PORT                       = "8080"
     DOCKER_ENABLE_CI                    = "true"
-    SCM_DO_BUILD_DURING_DEPLOYMENT      = "false"
+    SCM_DO_BUILD_DURING_DEPLOYMENT     = "false"
   }
 
   lifecycle {
     ignore_changes = [
-      site_config[0].scm_type,  # Ignorar cambios en scm_type
-      tags                        # Ignorar cambios en tags
+      site_config[0].scm_type,
+      tags
     ]
   }
 
   tags = var.tags
 }
 
-# Crear un rol personalizado si es necesario
 resource "azurerm_role_definition" "webapp_role" {
   name        = "WebAppDeploymentRole-${random_id.unique.hex}"
   scope       = data.azurerm_resource_group.rg.id
@@ -114,7 +117,6 @@ resource "azurerm_role_definition" "webapp_role" {
   ]
 }
 
-# Asignar el rol a la entidad de servicio
 resource "azurerm_role_assignment" "example" {
   scope              = data.azurerm_resource_group.rg.id
   role_definition_id = azurerm_role_definition.webapp_role.id

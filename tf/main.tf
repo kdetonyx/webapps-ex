575a1d4e3befdc43f207f86f39c40cc953d9575f
Anthony Mogrovejo <kdetony@ballenaroja.services>
2025-07-07 14:02:20 -0500

    Update9 main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -5,6 +5,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.0.0"
+      configuration_aliases = [azurerm.management]
     }
     random = {
       source  = "hashicorp/random"
@@ -14,7 +15,14 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  
+  # Registra automáticamente los proveedores necesarios
+  resource_provider_registrations = ["Microsoft.Web", "Microsoft.Insights"]
 }
 
 variable "tags" {
@@ -45,45 +53,76 @@ resource "random_id" "unique" {
   byte_length = 4
 }
 
+resource "azurerm_application_insights" "appinsights" {
+  name                = "appi-webapp-${random_id.unique.hex}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
 resource "azurerm_app_service" "webapp" {
-  name                = "webapp-github-${random_id.unique.hex}"
+  name                = "webapp-${random_id.unique.hex}"  # Nombre más corto
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = data.azurerm_app_service_plan.asp.id
 
   site_config {
     linux_fx_version = "DOCKER|${var.docker_image}"
-    always_on        = false  # Requerido para el plan Free
+    always_on        = false
+    health_check_path = "/"  # Ruta de health check obligatoria
     
-    # Configuraciones adicionales para contenedores
-    health_check_path = "/"  # Ruta de health check
-    ftps_state        = "Disabled"  # Deshabilitar FTP
+    # Configuración de contenedores
+    container_registry_use_managed_identity = true
   }
 
   identity {
-    type = "SystemAssigned"  # Identidad administrada
+    type = "SystemAssigned"
   }
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"  # URL correcta
+    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"
+    APPINSIGHTS_INSTRUMENTATIONKEY      = azurerm_application_insights.appinsights.instrumentation_key
     
     # Configuraciones recomendadas
-    WEBSITES_CONTAINER_START_TIME_LIMIT = "600"
+    WEBSITES_PORT                       = "8080"  # Puerto que usa tu contenedor
     DOCKER_ENABLE_CI                    = "true"
-    
-    # Si tu imagen es privada, descomenta estas líneas:
-    # DOCKER_REGISTRY_SERVER_USERNAME   = "kdetony"
-    # DOCKER_REGISTRY_SERVER_PASSWORD   = "@Microsoft.KeyVault(SecretUri=https://kv-name.vault.azure.net/secrets/docker-pwd/version)"
+    SCM_DO_BUILD_DURING_DEPLOYMENT      = "false"
   }
 
-   tags = var.tags
+  lifecycle {
+    ignore_changes = [
+      site_config[0].scm_type,  # Ignorar cambios en scm_type
+      tags                        # Ignorar cambios en tags
+    ]
+  }
 
-  # Dependencia explícita en el App Service Plan
-  depends_on = [data.azurerm_app_service_plan.asp]
+  tags = var.tags
 }
 
-# Registrar el provider de Microsoft.Web si es necesario
-resource "azurerm_resource_provider_registration" "web" {
-  name = "Microsoft.Web"
+# Crear un rol personalizado si es necesario
+resource "azurerm_role_definition" "webapp_role" {
+  name        = "WebAppDeploymentRole-${random_id.unique.hex}"
+  scope       = data.azurerm_resource_group.rg.id
+  description = "Custom role for App Service deployment"
+
+  permissions {
+    actions = [
+      "Microsoft.Web/sites/*",
+      "Microsoft.Insights/components/*",
+      "Microsoft.Resources/subscriptions/resourceGroups/read"
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_resource_group.rg.id
+  ]
+}
+
+# Asignar el rol a la entidad de servicio
+resource "azurerm_role_assignment" "example" {
+  scope              = data.azurerm_resource_group.rg.id
+  role_definition_id = azurerm_role_definition.webapp_role.id
+  principal_id       = var.service_principal_object_id
 }
